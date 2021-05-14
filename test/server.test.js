d3f12c3821e3a4e4b60f482fce38be30fdfbfaa6
LucasJunio <lucas.verdine@gmail.com>
2021-05-14 10:17:39 -0300

    first commit

---
+++ test/server.test.js
@@ -0,0 +1,8 @@
+const supertest = require('supertest')
+
+const request = supertest('http://localhost:3000')
+
+test('Deve responder na porta 3000', () => {
+    return request.get('/')
+        .then(res => expect(res).toBe("Hello"))
+})
