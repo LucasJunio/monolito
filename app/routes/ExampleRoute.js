50fb083e035122358172895f24861db20087f4c2
LucasJunio <lucas.verdine@gmail.com>
2021-05-17 15:30:44 -0300

    work in progress arq

--- app/routes/ExampleRoute.js
+++ app/routes/ExampleRoute.js
@@ -1,8 +1,8 @@
-import ExampleController from '../controllers/ExampleController'
-import CommonRoute from './CommonRoute'
+// import ExampleController from '../controllers/ExampleController'
+// import CommonRoute from './CommonRoute'
 
-export default class ExampleRoute extends CommonRoute {
-  constructor(app) {
-    super(ExampleController, app, 'example')
-  }
-}
+// export default class ExampleRoute extends CommonRoute {
+//   constructor(app) {
+//     super(ExampleController, app, 'example')
+//   }
+// }
