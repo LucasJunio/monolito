e64e55cb9c890b2eedf86683e80f93538220935e
LucasJunio <lucas.verdine@gmail.com>
2021-05-19 15:12:33 -0300

    clear develop

--- test/server.test.js
+++
@@ -1,8 +0,0 @@
-const supertest = require('supertest')
-
-const request = supertest('http://localhost:3000')
-
-test('Deve responder na porta 3000', () => {
-    return request.get('/')
-        .then(res => expect(res).toBe("Hello"))
-})
