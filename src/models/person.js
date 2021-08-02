dcda828bc825b6a56e9ec644b85b9d48ae863eaa
LucasJunio <lucas.verdine@gmail.com>
2021-08-02 11:04:50 -0300

    comment person

--- src/models/person.js
+++ src/models/person.js
@@ -1,35 +1,35 @@
-require('dotenv').config()
-const sql = require("mssql");
-const { config } = require('../config/settings');
+// require('dotenv').config()
+// const sql = require("mssql");
+// const { config } = require('../config/settings');
 
-const { cellphoneSchema } = require('../validate/person.validation');
+// const { cellphoneSchema } = require('../validate/person.validation');
 
-async function updateCellphone(payload) {
+// async function updateCellphone(payload) {
 
-    return new Promise(async function (resolve, reject) {
+//     return new Promise(async function (resolve, reject) {
 
-        sql.connect(config, async function (err) {
+//         sql.connect(config, async function (err) {
 
-            if (err) return reject({ name: 'Conexão com o banco de dados falhou.', message: err })
+//             if (err) return reject({ name: 'Conexão com o banco de dados falhou.', message: err })
 
-            cellphoneSchema(payload)
-                .then(result => {
+//             cellphoneSchema(payload)
+//                 .then(result => {
 
-                    let request = new sql.Request();
+//                     let request = new sql.Request();
 
-                    await request.query(`update ='${payload.celular}'`, async function (err, recordset) {
+//                     await request.query(`update ='${payload.celular}'`, async function (err, recordset) {
 
-                        sql.close();
+//                         sql.close();
 
-                        if (err) return reject({ name: 'Token SMS não encontrado.', message: err })
+//                         if (err) return reject({ name: 'Token SMS não encontrado.', message: err })
 
-                        return resolve()
-                    });
+//                         return resolve()
+//                     });
 
-                })
-                .catch(err => reject(err));
-        });
-    });
-}
+//                 })
+//                 .catch(err => reject(err));
+//         });
+//     });
+// }
 
-module.exports = { updateCellphone }
+// module.exports = { updateCellphone }
