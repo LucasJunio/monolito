394a15cea091bb68dd0004783a2efbc9b612d4c3
LucasJunio <lucas.verdine@gmail.com>
2021-08-12 13:38:03 -0300

    feat: :construction: lju #767

--- src/validate/user.validation.js
+++ src/validate/user.validation.js
@@ -1,18 +1,5 @@
 const Joi = require('joi');
 
-async function validateUser(payload) {
-
-  return new Promise(async function (resolve, reject) {
-
-    const { error } = await userSchema.validate(payload)
-
-    if (error)
-      return reject({ name: 'Falha na validação dos dados.', message: error })
-
-    return resolve()
-  })
-}
-
 const userSchema = Joi.object().keys({
   
   nome: Joi.string().min(10).max(150).required().messages({
@@ -36,7 +23,44 @@ const userSchema = Joi.object().keys({
     'string.empty': `Senha não deve ser um campo vazio.`,
     'any.required': `Senha é um campo requerido`
   }),
-
 })
 
-module.exports = { validateUser, userSchema }
+const userAdminSchema = Joi.object().keys({
+  nome: Joi.string().min(10).max(150).required().messages({
+    'string.base': `Nome deve ser do tipo texto.`,
+    'string.empty': `Nome não deve ser um campo vazio.`,
+    'string.max': `Nome deve ter no máximo {#limit} caracteres.`,
+    'string.min': `Nome deve ter no mínimo {#limit} caracteres.`,
+    'any.required': `Nome é um campo requerido`
+  }),
+  email: Joi.string().min(10).max(150).required().messages({
+    'string.base': `Email deve ser do tipo texto.`,
+    'string.empty': `Email não deve ser um campo vazio.`,
+    'string.max': `Email deve ter no máximo {#limit} caracteres.`,
+    'string.min': `Email deve ter no mínimo {#limit} caracteres.`,
+    'any.required': `Email é um campo requerido`
+  }),
+  senha: Joi.string().min(1).max(1000).required().messages({
+    'string.base': `Senha deve ser do tipo texto.`,
+    'string.min': `Senha deve ter no mínimo {#limit} caracteres.`,
+    'string.max': `Senha deve ter no máximo {#limit} caracteres.`,
+    'string.empty': `Senha não deve ser um campo vazio.`,
+    'any.required': `Senha é um campo requerido`
+  }),
+  cpf: Joi.string().min(11).max(45).required().messages({
+    'string.base': `CPF deve ser do tipo número (string).`,
+    'string.empty': `CPF não deve ser um campo vazio.`,
+    'string.max': `CPF deve ter no máximo {#limit} caracteres.`,
+    'string.min': `CPF deve ter no mínimo {#limit} caracteres.`,
+    'any.required': `CPF é um campo requerido`
+  }),
+  status: Joi.string().min(1).max(45).required().messages({
+    'string.base': `Status deve ser do tipo número (string).`,
+    'string.empty': `Status não deve ser um campo vazio.`,
+    'string.max': `Status deve ter no máximo {#limit} caracteres.`,
+    'string.min': `Status deve ter no mínimo {#limit} caracteres.`,
+    'any.required': `Status é um campo requerido`
+  }),
+})
+
+module.exports = { userSchema, userAdminSchema }
