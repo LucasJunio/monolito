443d477556136f4a09b3ff6bbd4d4a61a074b319
albertferreira <albert.ferreira@vileve>
2021-09-13 15:43:15 -0300

    correction url email front

--- src/models/validation.js
+++ src/models/validation.js
@@ -16,6 +16,9 @@ async function sendEmail(payload) {
       {}
     );
 
+    // ###########IMPORTANTE ALTERAR URL PARA HTTPS POIS AINDA ESTÁ HTTP
+    const urlfront = 'http://vileveway-develop.s3-website-us-east-1.amazonaws.com/email/' //URL de ROTA EMAIL FRONT
+
     const message = {
       from: "contato@vilevepay.com.br",
       to: payload.email,
@@ -23,7 +26,7 @@ async function sendEmail(payload) {
       html: `
             Olá ${payload.nome}, <br>
             <h2>Seja bem vindo ao gateway de pagamentos vileve.</h2> <br> Clique no link abaixo para confirmar sua conta.
-            <br> <a href='https://vileveway-backend-lb-develop.vileveway.com.br/api/v1/email/${token}'>Clique para confirmar sua conta</a> <br>  `,
+            <br> <a href='${urlfront}${token}'>Clique para confirmar sua conta</a> <br>  `,
     };
 
     email.sendMail(message, function (err, info) {
