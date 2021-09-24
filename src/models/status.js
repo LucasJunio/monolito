90b8beae17cdfb496baca0b0656ae1a2d98dfb77
Carlos Santos <carlos.santos@vileve.com.br>
2021-09-24 14:08:07 -0300

    fix: :bug: fixed the bug in status

--- src/models/status.js
+++ src/models/status.js
@@ -1,47 +1,108 @@
 const sql = require("mssql");
 const { config } = require("../config/settings");
-const { resolve } = require("mssql/lib/connectionstring");
-const { reject } = require("bcrypt/promises");
-function getStatus({ type }) {
-  return new Promise(async (reject, resolve) => {
+// const { resolve } = require("mssql/lib/connectionstring");
+// const { reject } = require("bcrypt/promises");
+
+const getStatus = ({ type }) => {
+  return new Promise(async (resolve, reject) => {
     try {
-      sql.connect(config, async (err) => {
-        if (err)
-          return reject({
-            name: "error",
-            message: "Conexão com o banco de dados falhou.",
-            details: err,
-          });
+      const pool = await sql.connect(config);
+      const result1 = await pool
+        .request()
+        .query(`select * from status where tipo = '${type}'`);
+      resolve({ name: "success", message: result1 });
+      pool.close();
 
-        const request = new sql.Request();
+      // const connStatus = new sql.Connection(config);
+      // const conn = await connStatus.connect();
+      // const request = new sql.Request(conn);
+      // request.query(
+      //   `select * from status where tipo = '${type}'`,
+      //   async (err, recordset) => {
+      //     if (err) reject({ name: "error", message: err });
+      //     resolve({ name: "success", message: recordset });
+      //   }
+      // );
+      // conn.close();
 
-        request.query(
-          `select * from status where tipo = '${type}'`,
-          async (err, recordset) => {
-            sql.close();
-            console.log(recordset);
-            if (err || recordset.length === 0)
-              return reject({
-                name: "error",
-                message: "Status não encontrado",
-                details: !!err
-                  ? "Syntax error: " + err.message
-                  : "rowsAffected: " + recordset.length,
-              });
-            return resolve({
-              name: "sucess",
-              message: {
-                status: recordset,
-              },
-            });
-          }
-        );
-      });
+      // sql.connect(config, async (err) => {
+      //   if (err)
+      //     return reject({
+      //       name: "error",
+      //       message: "Conexão com o banco de dados falhou.",
+      //       details: err,
+      //     });
+
+      //   const request = new sql.Request();
+
+      //   request.query(
+      //     `select * from status where tipo = '${type}'`,
+      //     async (err, recordset) => {
+      //       // sql.close();
+      //       console.log(recordset);
+      //       if (err || recordset.length === 0)
+      //         return reject({
+      //           name: "error",
+      //           message: "Status não encontrado",
+      //           details: !!err
+      //             ? "Syntax error: " + err.message
+      //             : "rowsAffected: " + recordset.length,
+      //         });
+      //       return resolve({
+      //         name: "sucess",
+      //         message: {
+      //           status: recordset,
+      //         },
+      //       });
+      //     }
+      //   );
+      // });
     } catch (error) {
       reject(error);
     }
   });
-}
+};
+
+// function getStatus({ type }) {
+//   return new Promise(async (reject, resolve) => {
+//     try {
+//       sql.connect(config, async (err) => {
+//         if (err)
+//           return reject({
+//             name: "error",
+//             message: "Conexão com o banco de dados falhou.",
+//             details: err,
+//           });
+
+//         const request = new sql.Request();
+
+//         request.query(
+//           `select * from status where tipo = '${type}'`,
+//           async (err, recordset) => {
+//             // sql.close();
+//             console.log(recordset);
+//             if (err || recordset.length === 0)
+//               return reject({
+//                 name: "error",
+//                 message: "Status não encontrado",
+//                 details: !!err
+//                   ? "Syntax error: " + err.message
+//                   : "rowsAffected: " + recordset.length,
+//               });
+//             return resolve({
+//               name: "sucess",
+//               message: {
+//                 status: recordset,
+//               },
+//             });
+//           }
+//         );
+//       });
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+// }
 
 module.exports = {
   getStatus,
