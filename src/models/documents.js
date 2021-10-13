d7ef40876e29e48f3428a54521968c624e861ffa
albertferreira <albert.ferreira@vileve>
2021-10-13 11:52:56 -0300

    include relation documents in route get documents id

--- src/models/documents.js
+++ src/models/documents.js
@@ -8,7 +8,26 @@ async function readDocuments(id) {
             const connuser = new sql.Connection(config);
             connuser.connect().then(() => {
                 var req = new sql.Request(connuser);
-                req.query(`select * from documentos where id_usuario = ${id}`, async (err, recordset) => {
+                req.query(`
+                
+                    SELECT doc.id
+                        ,doc.data
+                        ,doc.produto
+                        ,doc.id_usuario
+                        ,doc.nome
+                        ,re.categoria
+                        ,doc.alterado_por
+                        ,doc.status
+                        ,doc.data_status
+                        ,doc.base64
+                        ,doc.binario
+                    FROM relacao_documentos re
+                    join View_Cadastro_Lojista vw on vw.tipo_pessoa = re.pessoa
+                    left join documentos doc on (doc.id_usuario = vw.id_usuario and doc.categoria = re.categoria)
+                    where vw.id_usuario = ${id}
+
+
+                `, async (err, recordset) => {
                     if (err) reject({ name: "error", message: err });
                     !!recordset[0] ? resolve({ name: "success", message: recordset }) : resolve({ name: "success", message: 'Não há arquivos para este usuário!' });
                 });
