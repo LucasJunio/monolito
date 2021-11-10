bd31150ee59b749ce6b935d6d23e8a74b7ece3d0
albertferreira <albert.ferreira@vileve>
2021-11-10 16:26:14 -0300

    change filesize to 8Mb

--- src/upload/index.js
+++ src/upload/index.js
@@ -15,7 +15,7 @@ const storage = multer.memoryStorage()
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1024 * 1024,
+        fileSize: 1024 * 8192,
     },
     fileFilter: (req, file, cb) => {
         if (
